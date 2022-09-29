3dbbc5b41069c1d434e469adfcffc9687fba7015
Pierre ZETTL <107761093+PierreZETTL@users.noreply.github.com>
2022-09-29 22:05:16 +0200

    Initial Commit

---
+++ SunCloud/SunCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunCloudApp.swift
+//  SunCloud
+//
+//  Created by Pierre ZETTL on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SunCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
