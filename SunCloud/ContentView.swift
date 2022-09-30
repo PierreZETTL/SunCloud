1fa1194c60f9b875ab127e4e0db318f92ed45969
Pierre ZETTL <107761093+PierreZETTL@users.noreply.github.com>
2022-09-30 12:04:04 +0200

    Improve UI

--- SunCloud/ContentView.swift
+++ SunCloud/ContentView.swift
@@ -11,51 +11,58 @@ import CoreLocation
 struct ContentView: View {
     @State var weather: Weather = Weather(latitude: 10.0, longitude: 10.0, hourly: HourlyData(time: ["2022-07-01T00:00"], temperature_2m: [15]), current_weather: CurrentData(time: "test", temperature: 0.0))
     
-    @State var previsions = [Int]()
+    @State var previsionsbh = [Int]()
         
     var body: some View {
-        VStack {
-            Text("Localisation actuelle")
-                .font(.system(size: 25))
-                .fontWeight(.semibold)
-                .padding(.top)
-            Text("\(String(format: "%.0f", weather.current_weather.temperature))°")
-                .font(.system(size: 45))
-            Text("Coordonnées : \(String(format: "%.2f", weather.latitude)) / \(String(format: "%.2f", weather.longitude))")
-                .font(.system(size: 15))
-            List {
-                Section("🕦 Prévisions sur 4 heures") {
-                    HStack(alignment: .center) {
-                        Spacer()
-                        ForEach(previsions, id: \.self) { i in
-                            VStack(alignment: .center) {
-                                if i == Calendar.current.component(.hour, from: Date()) {
-                                    Text("Act.")
+        ZStack {
+            Color.blue.opacity(0.05)
+                .ignoresSafeArea()
+            VStack {
+                Text("Localisation actuelle")
+                    .font(.system(size: 25))
+                    .fontWeight(.semibold)
+                    .padding(.top)
+                Text("\(String(format: "%.0f", weather.current_weather.temperature))°")
+                    .font(.system(size: 45))
+                Text("Coordonnées : \(String(format: "%.2f", weather.latitude)) / \(String(format: "%.2f", weather.longitude))")
+                    .font(.system(size: 15))
+                List {
+                    Section("🕦 Prévisions sur 4 heures") {
+                        HStack(alignment: .center) {
+                            Spacer()
+                            ForEach(previsionsbh, id: \.self) { i in
+                                VStack(alignment: .center) {
+                                    if i == Calendar.current.component(.hour, from: Date()) {
+                                        Text("Act.")
+                                            .padding(.horizontal, 10.0)
+                                    } else {
+                                        Text("\(i) h")
+                                            .padding(.horizontal, 10.0)
+                                    }
+                                    Image(systemName: "cloud.sun.fill")
                                         .padding(.horizontal, 10.0)
-                                } else {
-                                    Text("\(i) h")
+                                        .padding(.vertical, 1.0)
+                                    Text("\(String(format: "%.0f", weather.hourly.temperature_2m[i]))°")
                                         .padding(.horizontal, 10.0)
                                 }
-                                Text("\(String(format: "%.0f", weather.hourly.temperature_2m[i]))°")
-                                    .padding(.horizontal, 10.0)
                             }
+                            Spacer()
                         }
-                        Spacer()
                     }
-                }
-                .listRowBackground(Color.blue.opacity(0.25))
-                Section("📆 Prévisions sur 5 jours") {
-                    Text("Test")
-                    Text("Test")
-                }
-                .listRowBackground(Color.blue.opacity(0.25))
-            }.scrollContentBackground(.hidden)
+                    .listRowBackground(Color.blue.opacity(0.15))
+                    Section("📆 Prévisions sur 5 jours") {
+                        Text("Test")
+                        Text("Test")
+                    }
+                    .listRowBackground(Color.blue.opacity(0.15))
+                }.scrollContentBackground(.hidden)
+            }
         }
         .onAppear {
             CLLocationManager().requestWhenInUseAuthorization()
             WeatherAPI().loadData { (weather) in
                 self.weather = weather
-                self.previsions = [Calendar.current.component(.hour, from: Date()), Calendar.current.component(.hour, from: Date())+1, Calendar.current.component(.hour, from: Date())+2, Calendar.current.component(.hour, from: Date())+3, Calendar.current.component(.hour, from: Date())+4]
+                self.previsionsbh = [Calendar.current.component(.hour, from: Date()), Calendar.current.component(.hour, from: Date())+1, Calendar.current.component(.hour, from: Date())+2, Calendar.current.component(.hour, from: Date())+3, Calendar.current.component(.hour, from: Date())+4]
             }
         }
     }
