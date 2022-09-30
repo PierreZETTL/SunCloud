b2e259a68eda2d0a7057f90e09f94ec5c4daa128
Pierre ZETTL <107761093+PierreZETTL@users.noreply.github.com>
2022-09-30 09:04:41 +0200

    Implement API

--- SunCloud/ContentView.swift
+++ SunCloud/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var weather: Weather = Weather(latitude: 10.0, longitude: 10.0, hourly: HourlyData(time: ["2022-07-01T00:00"], temperature_2m: [15]))
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("☀️ SunCloud ☁️")
+                .font(.system(size: 45))
+            
+            Spacer()
+            Text("Latitude : \(weather.latitude)")
+            Text("Longitude : \(weather.longitude)")
+            Spacer()
+            Text("Température : \(weather.hourly.temperature_2m[0])")
+            Text("À : \(weather.hourly.time[0])")
+            Spacer()
+            Spacer()
         }
         .padding()
+        .onAppear {
+            WeatherAPI().loadData { (weather) in
+                self.weather = weather
+            }
+        }
     }
 }
 
---
+++ SunCloud/WeatherAPI.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherAPI.swift
+//  SunCloud
+//
+//  Created by Pierre ZETTL on 30/09/2022.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let latitude: Float
+    let longitude: Float
+    let hourly: HourlyData
+}
+
+struct HourlyData: Codable {
+    let time: [String]
+    let temperature_2m: [Float]
+}
+
+class WeatherAPI : ObservableObject{
+    @Published var weather: Weather = Weather(latitude: 10.0, longitude: 10.0, hourly: HourlyData(time: ["2022-07-01T00:00"], temperature_2m: [15]))
+    
+    func loadData(completion:@escaping (Weather) -> ()) {
+        guard let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=Europe%2FParis") else {
+            print("URL invalide.")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let weather = try! JSONDecoder().decode(Weather.self, from: data!)
+            print(weather)
+            DispatchQueue.main.async {
+                completion(weather)
+            }
+        }.resume()
+        
+    }
+}
