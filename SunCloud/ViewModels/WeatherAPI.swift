a37f88f6b94d28b43013dd47c8ec125d783db0be
Pierre ZETTL <107761093+PierreZETTL@users.noreply.github.com>
2022-10-05 21:54:17 +0200

    Fix typo

--- SunCloud/ViewModels/WeatherAPI.swift
+++ SunCloud/ViewModels/WeatherAPI.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreLocation
 
-class WeatherAPI : ObservableObject{
+class WeatherAPI : ObservableObject {
     @Published var weather: Weather = Weather(latitude: 10.0, longitude: 10.0, hourly: HourlyData(time: ["2022-07-01T00:00"], temperature_2m: [15], rain: [0.0], cloudcover: [0.0], snowfall: [0.0]), daily: DailyData(time: ["2022-09-30"], temperature_2m_max: [6.5], temperature_2m_min: [4.5], rain_sum: [0.0], snowfall_sum: [0.0]), current_weather: CurrentData(time: "test", temperature: 0.0))
     
     func loadData(completion:@escaping (Weather) -> ()) {
