86ea2ea5aec63ad25ba42bba76bfc039dae5c086
Pierre ZETTL <107761093+PierreZETTL@users.noreply.github.com>
2022-10-08 09:33:48 +0200

    Add tests

--- SunCloudTests/SunCloudTests.swift
+++ SunCloudTests/SunCloudTests.swift
@@ -9,6 +9,21 @@ import XCTest
 @testable import SunCloud
 
 final class SunCloudTests: XCTestCase {
+    
+    var weather: Weather?
+    
+    override func setUp() {
+        super.setUp()
+        do {
+            let testBundle = Bundle(for: type(of: self))
+            if let url = testBundle.url(forResource: "Weather", withExtension: "json") {
+                let jsonData = try Data(contentsOf: url)
+                self.weather = try JSONDecoder().decode(Weather.self, from: jsonData)
+            }
+        } catch {
+            print("Erreur")
+        }
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,19 +33,88 @@ final class SunCloudTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testLatitude() throws {
+        XCTAssertEqual(weather?.latitude, 10.0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLongitude() throws {
+        XCTAssertEqual(weather?.longitude, 10.0)
+    }
+    
+    func testHourly() throws {
+        XCTAssertNotNil(weather?.hourly)
+    }
+    
+    func testHourlyTime() throws {
+        XCTAssertEqual(weather?.hourly.time[0], "2022-10-08T00:00")
+    }
+    
+    func testHourlyTemperature2M() throws {
+        XCTAssertEqual(weather?.hourly.temperature_2m[0], 24.2)
+    }
+    
+    func testHourlyRain() throws {
+        XCTAssertEqual(weather?.hourly.rain[0], 0.0)
+    }
+    
+    func testHourlyCloudcover() throws {
+        XCTAssertEqual(weather?.hourly.cloudcover[0], 100.0)
+    }
+    
+    func testHourlySnowfall() throws {
+        XCTAssertEqual(weather?.hourly.snowfall[0], 0.0)
+    }
+    
+    func testDaily() throws {
+        XCTAssertNotNil(weather?.daily)
+    }
+    
+    func testDailyTime() throws {
+        XCTAssertEqual(weather?.daily.time[0], "2022-10-08")
+    }
+    
+    func testDailyTemperature2mMax() throws {
+        XCTAssertEqual(weather?.daily.temperature_2m_max[0], 30.7)
+    }
+    
+    func testDailyTemperature2mMin() throws {
+        XCTAssertEqual(weather?.daily.temperature_2m_min[0], 22.2)
+    }
+    
+    func testDailyRainSum() throws {
+        XCTAssertEqual(weather?.daily.rain_sum[0], 0.0)
+    }
+    
+    func testDailySnowfallSum() throws {
+        XCTAssertEqual(weather?.daily.snowfall_sum[0], 0.0)
+    }
+    
+    func testDailySunrise() throws {
+        XCTAssertEqual(weather?.daily.sunrise[0], "2022-10-08T07:07")
+    }
+    
+    func testDailySunset() throws {
+        XCTAssertEqual(weather?.daily.sunset[0], "2022-10-08T19:07")
+    }
+    
+    func testDailyWindspeed10mMax() throws {
+        XCTAssertEqual(weather?.daily.windspeed_10m_max[0], 11.4)
+    }
+    
+    func testDailyWinddirection10mDominant() throws {
+        XCTAssertEqual(weather?.daily.winddirection_10m_dominant[0], 155.32315)
+    }
+    
+    func testCurrentWeather() throws {
+        XCTAssertNotNil(weather?.current_weather)
+    }
+    
+    func testCurrentWeatherTime() throws {
+        XCTAssertEqual(weather?.current_weather.time, "2022-10-08T08:00")
+    }
+    
+    func testCurrentWeatherTemperature() throws {
+        XCTAssertEqual(weather?.current_weather.temperature, 22.9)
     }
 
 }
--- SunCloudUITests/SunCloudUITests.swift
+++ SunCloudUITests/SunCloudUITests.swift
@@ -22,12 +22,69 @@ final class SunCloudUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testMaPosition() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.tabBars["Tab Bar"].buttons["Ma position"].tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["Mercredi"]/*[[".cells.staticTexts[\"Mercredi\"]",".staticTexts[\"Mercredi\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        app/*@START_MENU_TOKEN@*/.collectionViews.containing(.other, identifier:"Vertical scroll bar, 2 pages").element/*[[".collectionViews.containing(.other, identifier:\"Horizontal scroll bar, 1 page\").element",".collectionViews.containing(.other, identifier:\"Vertical scroll bar, 2 pages\").element"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["Lever du soleil"].swipeRight()/*[[".cells.staticTexts[\"Lever du soleil\"]",".swipeDown()",".swipeRight()",".staticTexts[\"Lever du soleil\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
+    }
+    
+    func testPositionAleatoire() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.tabBars["Tab Bar"].buttons["Position aléatoire"].tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 10).children(matching: .other).element(boundBy: 1).children(matching: .other).element.swipeUp()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 4).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.swipeDown()
+        
+        let refreshButton = app.buttons["Refresh"]
+        refreshButton.tap()
+    }
+    
+    func testCarte() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.tabBars["Tab Bar"].buttons["Carte"].tap()
+        
+        let positionActuelleButton = app.buttons["Position actuelle"]
+        positionActuelleButton.tap()
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.buttons["Position aléatoire"].tap()
+        positionActuelleButton.tap()
+    }
+    
+    func testRecherche() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.tabBars["Tab Bar"].buttons["Recherche"].tap()
+        
+        let textField = app.scrollViews.otherElements.containing(.staticText, identifier:"Recherche").children(matching: .textField).element
+        textField.tap()
+        
+        textField.typeText("Paris")
+        
+        app.buttons["Valider"].tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 6).children(matching: .other).element(boundBy: 1).children(matching: .other).element.tap()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 4).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.tap()
+        
     }
     
     func testLaunchPerformance() throws {
